fe92636769f3f614d7638e7c2072b29fed241524
Richard Onyon <onyon@Richards-MacBook-Pro.local>
2022-03-15 20:46:14 -0700

    Adding description to variable inputs.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,25 @@
-variable "certificate_arn" {}
-variable "domain" {}
-variable "name" {}
-variable "type" {}
-variable "record" {}
-variable "ttl" {}
+variable "certificate_arn" {
+  description = "Certificate ARN."
+}
+variable "domain" {
+  description = "Domain name to authenticate against."
+}
+variable "name" {
+  description = "Name of local DNS record."
+}
+variable "type" {
+  description = "DNS record type."
+}
+variable "record" {
+  description = "Certificate Manager DNS entry."
+}
+variable "ttl" {
+  description = "TTL for DNS record."
+}
 
 locals {
   certificate_arn = var.certificate_arn
-  domain          = replace(var.domain,"*.","")
+  domain          = replace(var.domain, "*.", "")
   name            = var.name
   type            = var.type
   record          = var.record
